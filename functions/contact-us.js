5cf54adbaa2f037efb9572bcd54600628b3cb1b0
Michal Nowikowski <godfryd@isc.org>
2023-08-21 22:29:57 +0200

    enabling contact form code

--- functions/contact-us.js
+++ functions/contact-us.js
@@ -1,8 +1,26 @@
-export function onRequest(context) {
-    return new Response('{"text": "Email!", "type": "ok"}',
-                        {
-                            headers: {
-                                "content-type": "application/json;charset=UTF-8",
-                            },
-                        })
-}
+// export function onRequest(context) {
+//     return new Response('{"text": "Wiadomość wysłana!", "type": "ok"}',
+//                         {
+//                             headers: {
+//                                 "content-type": "application/json;charset=UTF-8",
+//                             },
+//                         })
+// }
+
+import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+export const onRequest = mailchannelsPlugin({
+  personalizations: [
+    {
+      to: [{ name: "ACME Support", email: "godfryd@gmail.com" }],
+    },
+  ],
+  from: { name: "Enquiry", email: "no-reply@example.com" },
+  respondWith: () =>
+    new Response('{"text": "Wiadomość wysłana!", "type": "ok"}',
+                 {
+                     headers: {
+                         "content-type": "application/json;charset=UTF-8",
+                     },
+                 }),
+});
