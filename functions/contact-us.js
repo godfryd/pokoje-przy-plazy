a0cc44c5605d38a4dda19ba58a676327051b8404
Michal Nowikowski <godfryd@isc.org>
2023-08-21 22:57:30 +0200

    enabling contact form code

--- functions/contact-us.js
+++ functions/contact-us.js
@@ -7,20 +7,32 @@
 //                         })
 // }
 
-import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+// import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest = mailchannelsPlugin({
-  personalizations: [
-    {
-      to: [{ name: "ACME Support", email: "godfryd@gmail.com" }],
-    },
-  ],
-  from: { name: "Enquiry", email: "no-reply@example.com" },
-  respondWith: () =>
-    new Response('{"text": "Wiadomość wysłana!", "type": "ok"}',
-                 {
-                     headers: {
-                         "content-type": "application/json;charset=UTF-8",
-                     },
-                 }),
-});
+// export const onRequest = mailchannelsPlugin({
+//   personalizations: [
+//     {
+//       to: [{ name: "ACME Support", email: "godfryd@gmail.com" }],
+//     },
+//   ],
+//   from: { name: "Enquiry", email: "no-reply@example.com" },
+//   respondWith: () =>
+//     new Response('{"text": "Wiadomość wysłana!", "type": "ok"}',
+//                  {
+//                      headers: {
+//                          "content-type": "application/json;charset=UTF-8",
+//                      },
+//                  }),
+// });
+
+
+export default {
+  async fetch(request) {
+      return new Response('{"text": "Wiadomość wysłana!", "type": "ok"}',
+                          {
+                              headers: {
+                                  "content-type": "application/json;charset=UTF-8",
+                              },
+                          })
+  },
+};
