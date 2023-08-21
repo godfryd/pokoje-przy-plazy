3c9b84cb1a65a88aa22865429c54e4b030f8578e
Michal Nowikowski <godfryd@isc.org>
2023-08-21 23:06:09 +0200

    enabling contact form code

--- functions/contact-us.js
+++ functions/contact-us.js
@@ -1,11 +1,36 @@
-// export function onRequest(context) {
-//     return new Response('{"text": "Wiadomość wysłana!", "type": "ok"}',
-//                         {
-//                             headers: {
-//                                 "content-type": "application/json;charset=UTF-8",
-//                             },
-//                         })
-// }
+export function onRequest(context) {
+    var send_request = new Request('https://api.mailchannels.net/tx/v1/send', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            personalizations: [
+                {
+                    to: [{ email: 'godfryd@gmail.com', name: 'Test Recipient' }],
+                },
+            ],
+            from: {
+                email: 'sender@example.com',
+                name: 'Workers - MailChannels integration',
+            },
+            subject: 'Look! No servers',
+            content: [
+                {
+                    type: 'text/plain',
+                    value: 'And no email service accounts and all for free too!',
+                },
+            ],
+        }),
+    })
+
+    return new Response('{"text": "Wiadomość wysłana!", "type": "ok"}',
+                        {
+                            headers: {
+                                "content-type": "application/json;charset=UTF-8",
+                            },
+                        })
+}
 
 // import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
@@ -24,15 +49,3 @@
 //                      },
 //                  }),
 // });
-
-
-export default {
-  async fetch(request) {
-      return new Response('{"text": "Wiadomość wysłana!", "type": "ok"}',
-                          {
-                              headers: {
-                                  "content-type": "application/json;charset=UTF-8",
-                              },
-                          })
-  },
-};
