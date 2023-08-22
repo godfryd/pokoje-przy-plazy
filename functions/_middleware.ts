de641f8fc1e34075664300d96beba7ebb33ed464
Michal Nowikowski <godfryd@isc.org>
2023-08-23 00:20:01 +0200

    enabling contact form code

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,18 +1,18 @@
-import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+// import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest: PagesFunction = mailChannelsPlugin({
-    personalizations: [
-        {
-            to: [{ name: "ACME Support", email: "godfryd@gmail.com" }],
-        },
-    ],
-    from: {
-        name: "ACME Support",
-        email: "godfryd@gmail.com",
-    },
-    respondWith: () => {
-        return new Response(
-            `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
-        );
-    },
-});
+// export const onRequest: PagesFunction = mailChannelsPlugin({
+//     personalizations: [
+//         {
+//             to: [{ name: "ACME Support", email: "godfryd@gmail.com" }],
+//         },
+//     ],
+//     from: {
+//         name: "ACME Support",
+//         email: "godfryd@gmail.com",
+//     },
+//     respondWith: () => {
+//         return new Response(
+//             `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
+//         );
+//     },
+// });
--- functions/contact-us.js
+++ functions/contact-us.js
@@ -1,4 +1,41 @@
 export function onRequest(context) {
+    var payload = {
+        "personalizations": [
+            {
+                "to": [
+                    {
+                        "email": "godfryd@gmail.com"
+                    }
+                ],
+                "subject": "Hello, World!"
+            }
+        ],
+        "from": {
+            "email": "kontakt@pokojeprzyplazy.pl"
+        },
+        "content": [
+            {
+                "type": "text/plain",
+                "value": "Hello, World!"
+            }
+        ]
+    };
+    var myHeaders = new Headers({
+        "Content-Type": "application/json",
+        "Authorization": "Bearer YOUR_API_KEY",
+    });
+    var data = new FormData();
+    data.append( "json", JSON.stringify( payload ) );
+    fetch("https://api.sendgrid.com/v3/mail/send",
+          {
+              method: "POST",
+              headers: myHeaders,
+              body: data
+          })
+        .then(function(res){ return res.json(); })
+        .then(function(data){ console.log( JSON.stringify( data ) ) })
+
+
 
     return new Response('{"text": "Wiadomość wysłana!", "type": "ok"}',
                         {
